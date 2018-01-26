606003d652a843e5740a78456742521f2da4604a
Sam Allen <samuel.j.allen@icloud.com>
2018-01-25 19:40:43 -0500

    First commit. Need to debug. Error in re innerHTML per Chrome Dev Tools?

---
+++ README.md
@@ -0,0 +1,2 @@
+Inspired by Dave McFarland's lecture at:
+https://teamtreehouse.com/library/the-object-literal
---
+++ js/object.js
@@ -0,0 +1,15 @@
+var person = { /* The curly braces mean that an OBJECT is represented. Brackets would represent an array. */
+    name : 'Sam',
+    country : 'USA', /* Notice how you can use strings in an object's property? */
+    age : 33, /* Notice how you can use numbers in an object's property? */
+    CodeLouisville : true, /* Notice how you can use boolean true/false values in an object's property? */
+    skills : ['HTML', 'CSS', 'JavaScript'] /* Notice how you can put an array as an object's property? */
+};
+
+function print(message) {
+    var div = document.getElementById('output');
+    div.innerHTML = message;
+}
+
+var message = '<p>Hello. My name is ' + person.name + '</p>'; /* FYI, person.name is the "dot notation" way of accessing info in an object. The object is person and the property is name, the value of which is Sam. As an alternative to "dot notation", you also could have used "bracket notation" -- person['name'] instead of person.name -- to access the same info. */
+print(message); /* This just prints the above line's output to the page. */
